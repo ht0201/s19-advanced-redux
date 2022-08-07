0adc14470e60a16a6aa8a8c29c22734290a6a20e
ht0201 <43738511+ht0201@users.noreply.github.com>
2022-08-08 04:58:56 +0700

    handling error and notification

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { Fragment, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { uiActions } from "./store/reducers/ui-slice";
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const cardIsVisiable = useSelector((state) => state.ui.cardIsVisiable);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
   useEffect(() => {
-    fetch("https://react-http-8333c-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
-    });
-  }, [cart]);
+    const sendDataCart = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "Pending...",
+          title: "Sending...",
+          message: "Sending cart data!",
+        })
+      );
+      const res = await fetch(
+        "https://react-http-8333c-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!res.ok) {
+        throw new Error("Send data cart failed!");
+      }
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success",
+          message: "Data sent !",
+        })
+      );
+
+      // const data = await res.json();
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendDataCart().catch(() =>
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Failed",
+          message: "Send data cart failed !",
+        })
+      )
+    );
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {cardIsVisiable && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cardIsVisiable && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- src/store/reducers/ui-slice.js
+++ src/store/reducers/ui-slice.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialUIState = {
   cardIsVisiable: false,
+  notification: null,
 };
 
 const uiSlice = createSlice({
@@ -10,6 +11,13 @@ const uiSlice = createSlice({
   reducers: {
     toggle(state) {
       state.cardIsVisiable = !state.cardIsVisiable;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
